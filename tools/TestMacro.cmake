d9aa7bbb07c339b6c599e858cb05592218b9d916
Steve Peters <scpeters@openrobotics.org>
2024-01-24 10:21:22 -0800

    Update to support gazebo11 (#4)
    * check_test_ran.py: copy latest from gz-cmake
    * TestMacro.cmake: run check_test_ran with python3
    * Find only gazebo11
    
    Signed-off-by: Steve Peters <scpeters@openrobotics.org>

--- tools/TestMacro.cmake
+++ tools/TestMacro.cmake
@@ -31,7 +31,7 @@ macro (gz_build_tests)
 
     # Check that the test produced a result and create a failure if it didn't.
     # Guards against crashed and timed out tests.
-    add_test(check_${BINARY_NAME} ${PROJECT_SOURCE_DIR}/tools/check_test_ran.py
+    add_test(check_${BINARY_NAME} python3 ${PROJECT_SOURCE_DIR}/tools/check_test_ran.py
 	    ${CMAKE_BINARY_DIR}/test_results/${BINARY_NAME}.xml)
 
     # Convert junit file to csv and place in test_results in source folder.
