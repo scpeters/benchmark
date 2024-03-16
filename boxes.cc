0c01834d3107ea27f1fe8f6a8e8937db934585ba
yaswanth1701 <gonnayaswanth17@gmail.com>
2024-03-16 10:20:51 +0530

    changes to boxes.cc file
    Signed-off-by: yaswanth1701 <gonnayaswanth17@gmail.com>

--- boxes.cc
+++ boxes.cc
@@ -39,9 +39,9 @@ void BoxesTest::Boxes(const std::string& _physicsEngine, double _dt, int _modelC
 
   std::stringstream command;
 
-  command << "engine=" << _physicsEngine << " model_count=" << _modelCount << " collision=" << _collision
-          << " complex=" << _complex << " dt=" << _dt << " erb " << WORLDS_PATH << "/boxes.world.erb > " << WORLDS_PATH
-          << "/boxes.world";
+  command << "engine_arg=" << _physicsEngine << " count_arg=" << _modelCount << " collision_arg=" << _collision
+          << " complex_arg=" << _complex << " dt_arg =" << _dt << " erb " << WORLDS_PATH << "/boxes.world.erb > "
+          << WORLDS_PATH << "/boxes.world";
 
   // creating model with desired configuration
   auto model_check = system(command.str().c_str());
