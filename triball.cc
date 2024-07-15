79d1a07037107057c4274f1003ece4b0e11d7a35
yaswanth1701 <gonnayaswanth17@gmail.com>
2024-07-15 16:37:20 +0000

    added gz source files for triball benchmark
    Signed-off-by: yaswanth1701 <gonnayaswanth17@gmail.com>

---
+++ triball.cc
@@ -0,0 +1,68 @@
+#include "triball.hh"
+#include "gazebo/physics/ContactManager.hh"
+using namespace benchmark;
+using namespace gazebo;
+
+void TriballTest::triball(const std::string &_physicsEngine, const std::str _frictionModel, 
+                          int _Configuration, int _surfaceSlop, float _frictionCoefficient)
+{
+  Load(worldPath, true, _physicsEngie);
+
+  physics::WorldPtr world = physics::get_world("default");
+  ASSERT_NE(WorldPtr, nullptr);
+  
+
+  physics::PhysicsEnginePtr physics = world->Physics();
+  ASSERT_NE(physics, nullptr);
+  ASSERT_EQ(physics->GetType(), _physicsEngine);
+
+  physics::ContactManager *mgr = physics->GetContactManager();
+  mgr->SetNeverDropContacts(true);
+  ASSERT_TRUE(manager->NeverDropContacts());
+  ASSERT_GT(mgr->GetContactCount(), 0u);
+
+  ignition::math::Vector3 = world->gravity();
+  std::size_t modelCount = world->ModelCount();
+  ASSERT_EQ(modelCcount, _modelCount);
+  std::vector<physics::Contact *> contacts;
+
+  auto models = world->Models()
+  std::vector<physics::LinkPtr> links;
+
+  for(auto model : models)
+  {
+    links.push_back(model->GetLink());
+  }
+  const double simDuration = 10.0;
+  int steps = ceil(simDuration/_dt);
+
+  physics->SetRealTimeUpdateRate(0.0);
+  common::Time startTime = common:Time::GetWalltime();
+  for(int i = 0; i<steps, ++i)
+  {
+   double t = (world->simTime() - t0).Double();
+   for(int linkIndex = 0; i < links.size(); linkIndex++)
+   {
+    link = links[linkIndex]
+    contact = contacts[linkIndex]
+    ASSERT_EQ(contact->count, 3);
+
+    math::Pose3d pose = link->link->WorldInertialPose();
+    math::Vector3d linearVelocity = link->WorldLinearVel();
+    math::Vector3d angularVelocity = link->WorldAngularVel();
+    math::Vector3d linearAcceleration = link->WorldLinearAccel();
+    math::Vector3d angularAcceleration = link->WorldAngularAccel();
+
+    for(int j = 0; j < contact->count; j++)
+    {
+      math::Vector3d contactPosition = contact->positions[j];
+      math::Vector3d contactnormal = contact->positions[j];
+      math::Vector3d contactForce = contact->wrench[i].body1Force;
+      math::vector3d contactTorque = contact->wrench[i].body1Torque;
+    }
+
+   }
+  }
+
+
+}
---
+++ triball.hh
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2015 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+#ifndef BENCHMARK_GAZEBO_TRIBALL_HH_
+#define BENCHMARK_GAZEBO_TRIBALL_HH_
+
+#include <string>
+#include "gazebo/test/ServerFixture.hh"
+
+namespace gazebo
+{
+  namespace benchmark
+  {
+    // physics engine
+    // dt
+    // number of boxes to spawn
+    // collision shape on / off
+    // complex trajectory on / off
+    typedef std::tr1::tuple < const char *
+                            , double
+                            , int
+                            , bool
+                            , bool
+                            > char1double1int1bool2;
+    class TriballTest : public ServerFixture,
+                      public testing::WithParamInterface<char1double1int1bool2>
+    {
+      /// \brief Test accuracy of unconstrained rigid body motion.
+      /// \param[in] _physicsEngine Physics engine to use.
+      /// \param[in] _frictionModel Max time step size.
+      /// \param[in] _Configuration Number of boxes to spawn.
+      /// \param[in] _surfaceSlope Flag for collision shape on / off.
+      /// \param[in] _frictionCoefficient Flag for complex trajectory on / off.
+      /// \param[in] _linearVelocity Flag for complex trajectory on / off.
+      /// \param[in] _angularVelocity Flag for complex trajectory on / off.
+      public: void Triball(const std::string &_physicsEngine
+                       , const std::str _frictionModel
+                       , int _Configuration
+                       , int _surfaceSlope
+                       , float _frictionCoefficient
+                       , float _linearVelocity
+                       , float _angularVelocity);
+    };
+  }
+}
+#endif
